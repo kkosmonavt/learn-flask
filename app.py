fdfaac645769e5b898a764b7bcaf566478402b4d
Sanita <kksmnvtt@gmail.com>
2025-12-17 13:34:57 +0700

    :)

--- app.py
+++ app.py
@@ -85,4 +85,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         print("База данных и таблицы созданы!")
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5001, host='0.0.0.0')
